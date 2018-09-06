44121c31e303d00737b7c98faa63a4d9d153b93b
qk-mac15 <jnuc093@gmail.com>
2018-09-06 17:19:17 +0800

    改成 @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "sequenceGenerator")     @SequenceGenerator(name = "sequenceGenerator")

--- src/main/java/com/mycompany/myapp/domain/DBFile.java
+++ src/main/java/com/mycompany/myapp/domain/DBFile.java
@@ -18,7 +18,8 @@ public class DBFile implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "sequenceGenerator")
+    @SequenceGenerator(name = "sequenceGenerator")
     private Long id;
 
     @Column(name = "file_name")
